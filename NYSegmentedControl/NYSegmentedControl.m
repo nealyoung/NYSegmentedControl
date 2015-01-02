b72653437b46a66fe59e59abbbdac3f6c0f38871
Soren Ulrikkeholm <me@sore.nu>
2015-01-02 22:49:39 +0100

    Make sure corner radius is applied to selected segment indicator even when corner radius is applied before the selected segment indicator layer has a frame.

--- NYSegmentedControl/NYSegmentedControl.m
+++ NYSegmentedControl/NYSegmentedControl.m
@@ -129,6 +129,7 @@
     }
 
     self.selectedSegmentIndicator.frame = [self indicatorFrameForSegment:self.segments[self.selectedSegmentIndex]];
+    [self setCornerRadius:[self cornerRadius]];
 }
 
 - (void)drawRect:(CGRect)rect {
