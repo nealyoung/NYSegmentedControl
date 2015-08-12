90a019e9b8c2c87a634b47b9f3ed135e0f4fe499
Nealon Young <hi@nealyoung.me>
2015-08-11 20:54:06 -0700

    Fixed memory leak

--- NYSegmentedControl/NYSegmentLabel.m
+++ NYSegmentedControl/NYSegmentLabel.m
@@ -46,25 +46,26 @@
         CGContextSetFillColorWithColor(context, [self.alternativeTextColor CGColor]);
 
         // Path from mask
-        CGPathRef path = [self pathForRoundedRect:self.maskFrame radius:self.maskCornerRadius];
+        CGPathRef path;
+        
+        if (CGRectIsEmpty(rect)) {
+            path = CGPathCreateMutable();
+        } else {
+            UIBezierPath *roundRectBezierPath = [UIBezierPath bezierPathWithRoundedRect:self.maskFrame
+                                                                           cornerRadius:self.maskCornerRadius];
+            path = CGPathCreateCopy([roundRectBezierPath CGPath]);
+        }
+        
         CGContextAddPath(context, path);
-
+        
         // Fill the path
         CGContextFillPath(context);
+        CFRelease(path);
 
         // Clean up
         CGContextRestoreGState(context);
         CGImageRelease(mask);
     }
-}
-
-- (CGPathRef)pathForRoundedRect:(CGRect)rect radius:(CGFloat)radius {
-    if (CGRectIsEmpty(rect)) {
-        return CGPathCreateMutable();
-    }
-    
-    UIBezierPath* path = [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:radius];
-    return [path CGPath];
 }
 
 - (UIColor *)alternativeTextColor {
