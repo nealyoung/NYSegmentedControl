e0a47c8ebfc262b53d56a52dd242cca305892f90
Nealon Young <hi@nealyoung.me>
2017-05-29 11:33:16 -0700

    Rebuilt example project

--- NYSegmentedControlDemo/NYSegmentedControlDemo/DemoViewController.m
+++ NYSegmentedControlDemo/NYSegmentedControlDemo/DemoViewController.m
@@ -11,9 +11,7 @@
 
 @interface DemoViewController ()
 
-@property NYSegmentedControl *segmentedControl;
-@property UIView *visibleExampleView;
-@property NSArray *exampleViews;
+@property (nonatomic, strong) UIStackView *stackView;
 
 @end
 
@@ -21,90 +19,62 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.navigationController.navigationBar.translucent = NO;
-//    [self.navigationController.navigationBar setBarStyle:UIBarStyleBlack];
 
-    self.view.backgroundColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
-    //self.navigationController.navigationBar.barTintColor = [UIColor colorWithWhite:0.26f alpha:1.0f];
-    //self.navigationController.navigationBar.translucent = NO;
-    //self.extendedLayoutIncludesOpaqueBars = YES;
+    self.stackView = [[UIStackView alloc] init];
+    self.stackView.axis = UILayoutConstraintAxisVertical;
+    self.stackView.distribution = UIStackViewDistributionFillEqually;
+    [self.view addSubview:self.stackView];
+    self.stackView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.stackView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+    [self.stackView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+    [self.stackView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+    [self.stackView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
 
-    // Control in navigation bar
-    self.segmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"Light", @"Dark"]];
-    [self.segmentedControl addTarget:self action:@selector(segmentSelected) forControlEvents:UIControlEventValueChanged];
-    self.segmentedControl.titleFont = [UIFont fontWithName:@"AvenirNext-Medium" size:14.0f];
-    self.segmentedControl.titleTextColor = [UIColor colorWithWhite:0.5f alpha:1.0f];
-    self.segmentedControl.selectedTitleFont = [UIFont fontWithName:@"AvenirNext-DemiBold" size:14.0f];
-    self.segmentedControl.selectedTitleTextColor = [UIColor whiteColor];
-    self.segmentedControl.borderWidth = 1.0f;
-    self.segmentedControl.borderColor = [UIColor colorWithWhite:0.15f alpha:1.0f];
-    self.segmentedControl.drawsGradientBackground = YES;
-    self.segmentedControl.segmentIndicatorInset = 2.0f;
-    self.segmentedControl.segmentIndicatorGradientTopColor = [UIColor colorWithRed:0.30 green:0.50 blue:0.88f alpha:1.0f];
-    self.segmentedControl.segmentIndicatorGradientBottomColor = [UIColor colorWithRed:0.20 green:0.35 blue:0.75f alpha:1.0f];
-    self.segmentedControl.drawsSegmentIndicatorGradientBackground = YES;
-    self.segmentedControl.segmentIndicatorBorderWidth = 0.0f;
-    self.segmentedControl.selectedSegmentIndex = 0;
-    [self.segmentedControl sizeToFit];
-    self.navigationItem.titleView = self.segmentedControl;
-    
-    UIView *lightControlExampleView = [[UIView alloc] initWithFrame:self.view.bounds];
-    lightControlExampleView.backgroundColor = [UIColor colorWithWhite:0.96f alpha:1.0f];
-    self.visibleExampleView = lightControlExampleView;
-    [self.view addSubview:lightControlExampleView];
+    [self addSegmentedControlExample:[self graySegmentedControl] withBackgroundColor:[UIColor colorWithWhite:0.96f alpha:1.0f]];
+    [self addSegmentedControlExample:[self blueSegmentedControl] withBackgroundColor:[UIColor colorWithRed:0.36f green:0.64f blue:0.88f alpha:1.0f]];
+    [self addSegmentedControlExample:[self purpleSegmentedControl] withBackgroundColor:[UIColor colorWithWhite:0.24f alpha:1.0f]];
+    [self addSegmentedControlExample:[self switchSegmentedControl] withBackgroundColor:[UIColor colorWithWhite:0.18f alpha:1.0f]];
+}
 
-    UIStackView *stackView = [[UIStackView alloc] init];
-    stackView.axis = UILayoutConstraintAxisVertical;
-    stackView.distribution = UIStackViewDistributionFillEqually;
-    [lightControlExampleView addSubview:stackView];
-    stackView.translatesAutoresizingMaskIntoConstraints = NO;
-    [stackView.leadingAnchor constraintEqualToAnchor:lightControlExampleView.leadingAnchor].active = YES;
-    [stackView.trailingAnchor constraintEqualToAnchor:lightControlExampleView.trailingAnchor].active = YES;
-    [stackView.topAnchor constraintEqualToAnchor:lightControlExampleView.topAnchor].active = YES;
-    [stackView.bottomAnchor constraintEqualToAnchor:lightControlExampleView.bottomAnchor].active = YES;
+- (void)addSegmentedControlExample:(NYSegmentedControl *)segmentedControl withBackgroundColor:(UIColor *)backgroundColor {
+    UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth([UIScreen mainScreen].bounds), 44.0f)];
+    backgroundView.backgroundColor = backgroundColor;
+    [self.stackView addArrangedSubview:backgroundView];
 
-    UIView *instagramSegmentedControlBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth([UIScreen mainScreen].bounds), 44.0f)];
-    instagramSegmentedControlBackgroundView.backgroundColor = [UIColor colorWithWhite:0.96f alpha:1.0f];
-    [stackView addArrangedSubview:instagramSegmentedControlBackgroundView];
+    [backgroundView addSubview:segmentedControl];
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
+    [segmentedControl.centerXAnchor constraintEqualToAnchor:backgroundView.centerXAnchor].active = YES;
+    [segmentedControl.centerYAnchor constraintEqualToAnchor:backgroundView.centerYAnchor].active = YES;
+}
 
-    NYSegmentedControl *instagramSegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"Popular", @"Following", @"News"]];
-    instagramSegmentedControl.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
-    instagramSegmentedControl.segmentIndicatorBackgroundColor = [UIColor whiteColor];
-    instagramSegmentedControl.segmentIndicatorInset = 0.0f;
-    instagramSegmentedControl.titleTextColor = [UIColor lightGrayColor];
-    instagramSegmentedControl.selectedTitleTextColor = [UIColor darkGrayColor];
-    [instagramSegmentedControl sizeToFit];
-    [instagramSegmentedControlBackgroundView addSubview:instagramSegmentedControl];
-    instagramSegmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
-    [instagramSegmentedControl.centerXAnchor constraintEqualToAnchor:instagramSegmentedControlBackgroundView.centerXAnchor].active = YES;
-    [instagramSegmentedControl.centerYAnchor constraintEqualToAnchor:instagramSegmentedControlBackgroundView.centerYAnchor].active = YES;
+- (NYSegmentedControl *)graySegmentedControl {
+    NYSegmentedControl *graySegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"Popular", @"Following", @"News"]];
+    graySegmentedControl.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
+    graySegmentedControl.segmentIndicatorBackgroundColor = [UIColor whiteColor];
+    graySegmentedControl.segmentIndicatorInset = 0.0f;
+    graySegmentedControl.titleTextColor = [UIColor lightGrayColor];
+    graySegmentedControl.selectedTitleTextColor = [UIColor darkGrayColor];
 
+    return graySegmentedControl;
+}
 
-    UIView *foursquareSegmentedControlBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth([UIScreen mainScreen].bounds), 44.0f)];
-    foursquareSegmentedControlBackgroundView.backgroundColor = [UIColor colorWithRed:0.36f green:0.64f blue:0.88f alpha:1.0f];
-    [stackView addArrangedSubview:foursquareSegmentedControlBackgroundView];
+- (NYSegmentedControl *)blueSegmentedControl {
+    NYSegmentedControl *blueSegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"Nearby", @"Worldwide"]];
+    blueSegmentedControl.titleTextColor = [UIColor colorWithRed:0.38f green:0.68f blue:0.93f alpha:1.0f];
+    blueSegmentedControl.selectedTitleTextColor = [UIColor whiteColor];
+    blueSegmentedControl.segmentIndicatorBackgroundColor = [UIColor colorWithRed:0.38f green:0.68f blue:0.93f alpha:1.0f];
+    blueSegmentedControl.backgroundColor = [UIColor colorWithRed:0.31f green:0.53f blue:0.72f alpha:1.0f];
+    blueSegmentedControl.borderWidth = 0.0f;
+    blueSegmentedControl.segmentIndicatorBorderWidth = 0.0f;
+    blueSegmentedControl.segmentIndicatorInset = 2.0f;
+    blueSegmentedControl.segmentIndicatorBorderColor = self.view.backgroundColor;
+    blueSegmentedControl.cornerRadius = blueSegmentedControl.intrinsicContentSize.height / 2.0f;
+    blueSegmentedControl.usesSpringAnimations = YES;
 
-    NYSegmentedControl *foursquareSegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"Nearby", @"Worldwide"]];
-    foursquareSegmentedControl.titleTextColor = [UIColor colorWithRed:0.38f green:0.68f blue:0.93f alpha:1.0f];
-    foursquareSegmentedControl.selectedTitleTextColor = [UIColor whiteColor];
-    foursquareSegmentedControl.segmentIndicatorBackgroundColor = [UIColor colorWithRed:0.38f green:0.68f blue:0.93f alpha:1.0f];
-    foursquareSegmentedControl.backgroundColor = [UIColor colorWithRed:0.31f green:0.53f blue:0.72f alpha:1.0f];
-    foursquareSegmentedControl.borderWidth = 0.0f;
-    foursquareSegmentedControl.segmentIndicatorBorderWidth = 0.0f;
-    foursquareSegmentedControl.segmentIndicatorInset = 2.0f;
-    foursquareSegmentedControl.segmentIndicatorBorderColor = self.view.backgroundColor;
-    [foursquareSegmentedControl sizeToFit];
-    foursquareSegmentedControl.cornerRadius = CGRectGetHeight(foursquareSegmentedControl.frame) / 2.0f;
-    foursquareSegmentedControl.usesSpringAnimations = YES;
-    [foursquareSegmentedControlBackgroundView addSubview:foursquareSegmentedControl];
-    foursquareSegmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
-    [foursquareSegmentedControl.centerXAnchor constraintEqualToAnchor:foursquareSegmentedControlBackgroundView.centerXAnchor].active = YES;
-    [foursquareSegmentedControl.centerYAnchor constraintEqualToAnchor:foursquareSegmentedControlBackgroundView.centerYAnchor].active = YES;
+    return blueSegmentedControl;
+}
 
-    UIView *purpleSegmentedControlBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth([UIScreen mainScreen].bounds), 44.0f)];
-    purpleSegmentedControlBackgroundView.backgroundColor = [UIColor colorWithWhite:0.24f alpha:1.0f];
-    [stackView addArrangedSubview:purpleSegmentedControlBackgroundView];
-
+- (NYSegmentedControl *)purpleSegmentedControl {
     NYSegmentedControl *purpleSegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"Lists", @"Followers"]];
     purpleSegmentedControl.borderWidth = 2.0f;
     purpleSegmentedControl.borderColor = [UIColor colorWithWhite:0.15f alpha:1.0f];
@@ -122,41 +92,35 @@
     purpleSegmentedControl.drawsSegmentIndicatorGradientBackground = YES;
     purpleSegmentedControl.segmentIndicatorGradientTopColor = [UIColor colorWithRed:0.65f green:0.25f blue:0.95f alpha:1.0f];
     purpleSegmentedControl.segmentIndicatorGradientBottomColor = [UIColor colorWithRed:0.4f green:0.15f blue:0.8f alpha:1.0f];
-    [purpleSegmentedControlBackgroundView addSubview:purpleSegmentedControl];
-    purpleSegmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
     [purpleSegmentedControl.widthAnchor constraintEqualToConstant:200.0f].active = YES;
     [purpleSegmentedControl.heightAnchor constraintEqualToConstant:40.0f].active = YES;
-    [purpleSegmentedControl.centerXAnchor constraintEqualToAnchor:purpleSegmentedControlBackgroundView.centerXAnchor].active = YES;
-    [purpleSegmentedControl.centerYAnchor constraintEqualToAnchor:purpleSegmentedControlBackgroundView.centerYAnchor].active = YES;
 
-    UIView *darkControlExampleView = [[UIView alloc] initWithFrame:self.view.bounds];
-    darkControlExampleView.backgroundColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
-    darkControlExampleView.hidden = YES;
-    [self.view addSubview:darkControlExampleView];
+    return purpleSegmentedControl;
+}
 
-    // UISwitch style
-    NYSegmentedControl *roundedGreenSegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"On", @"Off"]];
-    roundedGreenSegmentedControl.borderWidth = 2.0f;
-    roundedGreenSegmentedControl.borderColor = [UIColor colorWithWhite:0.15f alpha:1.0f];
-    roundedGreenSegmentedControl.titleFont = [UIFont fontWithName:@"AvenirNext-Medium" size:16.0f];
-    roundedGreenSegmentedControl.titleTextColor = [UIColor colorWithWhite:0.3f alpha:1.0f];
-    roundedGreenSegmentedControl.selectedTitleFont = [UIFont fontWithName:@"AvenirNext-DemiBold" size:18.0f];
-    roundedGreenSegmentedControl.selectedTitleTextColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
-    roundedGreenSegmentedControl.drawsGradientBackground = YES;
-    roundedGreenSegmentedControl.gradientTopColor = [UIColor colorWithWhite:0.17f alpha:1.0f];
-    roundedGreenSegmentedControl.gradientBottomColor = [UIColor colorWithWhite:0.05f alpha:1.0f];
-    roundedGreenSegmentedControl.segmentIndicatorAnimationDuration = 0.2f;
-    roundedGreenSegmentedControl.segmentIndicatorInset = 6.0f;
-    roundedGreenSegmentedControl.drawsSegmentIndicatorGradientBackground = YES;
-    roundedGreenSegmentedControl.segmentIndicatorGradientTopColor = [UIColor colorWithRed:0.75f green:0.9f blue:0.4f alpha:1.0f];
-    roundedGreenSegmentedControl.segmentIndicatorGradientBottomColor = [UIColor colorWithRed:0.47f green:0.72f blue:0.29f alpha:1.0f];
-    roundedGreenSegmentedControl.segmentIndicatorBorderWidth = 0.0f;
-    roundedGreenSegmentedControl.frame = CGRectMake(0, 0, 140.0f, 70.0f);
-    roundedGreenSegmentedControl.cornerRadius = CGRectGetHeight(roundedGreenSegmentedControl.frame) / 2.0f;
-    roundedGreenSegmentedControl.center = CGPointMake(darkControlExampleView.center.x, darkControlExampleView.center.y + 50.0f);
-    [darkControlExampleView addSubview:roundedGreenSegmentedControl];
-    
-    self.exampleViews = @[lightControlExampleView, darkControlExampleView];
+- (NYSegmentedControl *)switchSegmentedControl {
+    NYSegmentedControl *switchSegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"On", @"Off"]];
+    switchSegmentedControl.borderWidth = 2.0f;
+    switchSegmentedControl.borderColor = [UIColor colorWithWhite:0.15f alpha:1.0f];
+    switchSegmentedControl.titleFont = [UIFont fontWithName:@"AvenirNext-Medium" size:16.0f];
+    switchSegmentedControl.titleTextColor = [UIColor colorWithWhite:0.3f alpha:1.0f];
+    switchSegmentedControl.selectedTitleFont = [UIFont fontWithName:@"AvenirNext-DemiBold" size:18.0f];
+    switchSegmentedControl.selectedTitleTextColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
+    switchSegmentedControl.drawsGradientBackground = YES;
+    switchSegmentedControl.gradientTopColor = [UIColor colorWithWhite:0.17f alpha:1.0f];
+    switchSegmentedControl.gradientBottomColor = [UIColor colorWithWhite:0.05f alpha:1.0f];
+    switchSegmentedControl.segmentIndicatorAnimationDuration = 0.2f;
+    switchSegmentedControl.segmentIndicatorInset = 6.0f;
+    switchSegmentedControl.drawsSegmentIndicatorGradientBackground = YES;
+    switchSegmentedControl.segmentIndicatorGradientTopColor = [UIColor colorWithRed:0.75f green:0.9f blue:0.4f alpha:1.0f];
+    switchSegmentedControl.segmentIndicatorGradientBottomColor = [UIColor colorWithRed:0.47f green:0.72f blue:0.29f alpha:1.0f];
+    switchSegmentedControl.segmentIndicatorBorderWidth = 0.0f;
+    switchSegmentedControl.cornerRadius = 35.0f;
+    switchSegmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
+    [switchSegmentedControl.widthAnchor constraintEqualToConstant:140.0f].active = YES;
+    [switchSegmentedControl.heightAnchor constraintEqualToConstant:70.0f].active = YES;
+
+    return switchSegmentedControl;
 }
 
 - (BOOL)prefersStatusBarHidden {
